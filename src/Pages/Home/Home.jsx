44a4695510e4aa6e504d39de6cb2f05f55b325d0
hrhabib07 <futureofme07@gmail.com>
2022-06-30 21:51:52 +0600

    testimonial added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import DentalCare from "./DentalCare";
 import Feathers from "./Feathers";
 import Info from "./Info";
 import Services from "./Services";
+import Testimonial from "./Testimonial";
 
 const Home = () => {
   return (
@@ -13,6 +14,7 @@ const Home = () => {
       <Services></Services>
       <DentalCare></DentalCare>
       <Feathers></Feathers>
+      <Testimonial></Testimonial>
     </div>
   );
 };
---
+++ src/Pages/Home/Testimonial.jsx
@@ -0,0 +1,87 @@
+import React from "react";
+import quote from "../../assets/icons/quote.svg";
+import people1 from "../../assets/images/people1.png";
+import people2 from "../../assets/images/people2.png";
+import people3 from "../../assets/images/people3.png";
+
+const Testimonial = () => {
+  return (
+    <div
+      className="my-8 px-12"
+      style={{
+        backgroundImage: `url(${quote})`,
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "right",
+      }}
+    >
+      <div>
+        <h2 className="text-secondary text-xl font-bold">Testimonial</h2>
+        <h2 className="text-2xl">What Our Patients Says</h2>
+      </div>
+      <div className="pt-12 grid grid-cols-1 md:grid-cols-3 gap-4">
+        <div class="card w-96 bg-base-100 shadow-xl">
+          <div class="card-body">
+            <p>
+              It is a long established fact that by the readable content of a
+              lot layout. The point of using Lorem a more-or-less normal
+              distribu to using Content here, content
+            </p>
+            <div className="mt-8 flex">
+              <div class="avatar pr-4">
+                <div class="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                  <img src={people2} alt="people2" />
+                </div>
+              </div>
+              <div className="pt-4">
+                <h2 className="text-2xl font-bold">Will D Lovely</h2>
+                <h3 className="text-xl ">NYC</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="card w-96 bg-base-100 shadow-xl">
+          <div class="card-body">
+            <p>
+              It is a long established fact that by the readable content of a
+              lot layout. The point of using Lorem a more-or-less normal
+              distribu to using Content here, content
+            </p>
+            <div className="mt-8 flex">
+              <div class="avatar pr-4">
+                <div class="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                  <img src={people3} alt="people3" />
+                </div>
+              </div>
+              <div className="pt-4">
+                <h2 className="text-2xl font-bold">Asma Khanom</h2>
+                <h3 className="text-xl ">South China</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="card w-96 bg-base-100 shadow-xl">
+          <div class="card-body">
+            <p>
+              It is a long established fact that by the readable content of a
+              lot layout. The point of using Lorem a more-or-less normal
+              distribu to using Content here, content
+            </p>
+            <div className="mt-8 flex">
+              <div class="avatar pr-4">
+                <div class="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                  <img src={people1} alt="people1" />
+                </div>
+              </div>
+              <div className="pt-4">
+                <h2 className="text-2xl font-bold">Winson Herry</h2>
+                <h3 className="text-xl ">California</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Testimonial;
