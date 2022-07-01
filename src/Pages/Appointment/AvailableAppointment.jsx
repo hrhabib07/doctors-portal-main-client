bab394a1a4635ed13679ab5574b2ac1ed9f43f35
hrhabib07 <futureofme07@gmail.com>
2022-07-01 23:00:43 +0600

    servicesjson loaded in available appointment

--- src/Pages/Appointment/AvailableAppointment.jsx
+++ src/Pages/Appointment/AvailableAppointment.jsx
@@ -1,7 +1,13 @@
 import { format } from "date-fns";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const AvailableAppointment = ({ date }) => {
+  const [services, setServices] = useState();
+  useEffect(() => {
+    fetch("services.json")
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, []);
   return (
     <div className="my-12">
       <h2 className="text-3xl text-secondary text-center">
