de16c7ee1ee92cec4a62e076330fac0928c9fcef
hrhabib07 <futureofme07@gmail.com>
2022-07-01 23:33:49 +0600

    appointment booking option added

--- src/Pages/Appointment/Service.jsx
+++ src/Pages/Appointment/Service.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 
 const Service = ({ service }) => {
-  console.log(service);
+  const { name, slots } = service;
   return (
-    <div class="card bg-base-100 drop-shadow-md text-center ">
+    <div class="card max-w-lg bg-base-100 shadow-xl">
       <div class="card-body ">
-        <h2 class="card-title text-secondary ">{service.name}</h2>
+        <h2 class="card-title ">{name}</h2>
+        <p className="mt-4">
+          {slots.length ? (
+            <span> {slots[0]} </span>
+          ) : (
+            <span className="text-red-500"> Try another date </span>
+          )}
+        </p>
+        <p className="mb-4">
+          {slots.length} {slots.length < 1 ? "space" : "spaces"} available
+        </p>
+        <div class="card-actions justify-center">
+          <button disabled={slots.length === 0} class="btn btn-primary">
+            Buy Now
+          </button>
+        </div>
       </div>
     </div>
   );
