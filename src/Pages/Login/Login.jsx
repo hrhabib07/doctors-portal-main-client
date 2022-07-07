9cb2d50ace9f9071e621a1b1100d98e2e866a5ed
hrhabib07 <futureofme07@gmail.com>
2022-07-07 12:04:19 +0600

    login inited

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Login = () => {
   return (
-    <div className="flex h-screen justify-items-centers align-center">
+    <div className="">
       <div class=" card w-96 bg-base-100 shadow-xl">
         <div class="card-body">
           <h2 class="text-3xl font-bold">Login</h2>
