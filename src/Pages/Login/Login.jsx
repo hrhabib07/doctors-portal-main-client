5015e95f3ede1c9f4e77523c50a72afb4ec72c38
hrhabib07 <futureofme07@gmail.com>
2022-07-12 15:21:16 +0600

    login page updated

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -2,11 +2,12 @@ import React from "react";
 
 const Login = () => {
   return (
-    <div className="">
+    <div className="flex h-screen justify-center items-center">
       <div class=" card w-96 bg-base-100 shadow-xl">
         <div class="card-body">
           <h2 class="text-3xl font-bold">Login</h2>
-          <p>hello</p>
+          <div class="divider">OR</div>
+          <button class="btn btn-outline btn-primary">Button</button>
         </div>
       </div>
     </div>
