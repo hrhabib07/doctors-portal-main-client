b694f09e6091f4052a5816d62e6949bd31a8d2bc
hrhabib07 <futureofme07@gmail.com>
2022-07-12 15:27:53 +0600

    login page updated

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -5,9 +5,9 @@ const Login = () => {
     <div className="flex h-screen justify-center items-center">
       <div class=" card w-96 bg-base-100 shadow-xl">
         <div class="card-body">
-          <h2 class="text-3xl font-bold">Login</h2>
+          <h2 class="text-center text-3xl font-bold">Login</h2>
           <div class="divider">OR</div>
-          <button class="btn btn-outline btn-primary">Button</button>
+          <button class="btn btn-outline">Continue with Google</button>
         </div>
       </div>
     </div>
